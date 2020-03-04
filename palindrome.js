fd009aaf9f749eedbfabcfc73a0a8ced9b3b59be
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-03-03 20:49:32 -0500

    finished

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,27 @@
 const palindrome = str => {
 
+  let re = /[\W_]/g;
+  let lowRegStr = str.toLowerCase().replace(re, '');
+  let reverseStr = lowRegStr.split('').reverse().join('');
+  return reverseStr === lowRegStr;
+
+
+
+  // console.log(str);
+
+
+  // str.replace(" ", "");
+
+  // str.spice
+
+  // let revString = str.reverse();
+  // string.innerHTML = revString;
+
+  // return revString === str;
+
+
+
+
 }
 
 console.log(palindrome("a man a plan a canal panama"))
